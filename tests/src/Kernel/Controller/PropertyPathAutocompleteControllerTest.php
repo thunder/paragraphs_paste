4d4d415e0a615661aab3511991775da6fe382ba4
Christian Fritsch <chr.fritsch@gmx.net>
2020-03-25 13:49:03 +0100

    refactor: use config in kernel test

--- tests/src/Kernel/Controller/PropertyPathAutocompleteControllerTest.php
+++ tests/src/Kernel/Controller/PropertyPathAutocompleteControllerTest.php
@@ -2,10 +2,7 @@
 
 namespace Drupal\Tests\paragraphs_paste\Kernel\Controller;
 
-use Drupal\field\Entity\FieldConfig;
-use Drupal\field\Entity\FieldStorageConfig;
 use Drupal\KernelTests\KernelTestBase;
-use Drupal\paragraphs\Entity\ParagraphsType;
 use Drupal\paragraphs_paste\Controller\PropertyPathAutocompleteController;
 use Drupal\Tests\media\Traits\MediaTypeCreationTrait;
 use Symfony\Component\HttpFoundation\Request;
@@ -25,12 +22,16 @@ class PropertyPathAutocompleteControllerTest extends KernelTestBase {
    * @var array
    */
   protected static $modules = [
+    'paragraphs_paste_test',
     'paragraphs_paste',
     'paragraphs',
+    'entity_reference_revisions',
     'file',
     'field',
     'media',
-    'image',
+    'node',
+    'system',
+    'text',
   ];
 
   /**
@@ -39,59 +40,7 @@ class PropertyPathAutocompleteControllerTest extends KernelTestBase {
   protected function setUp() {
     parent::setUp();
 
-    ParagraphsType::create([
-      'label' => 'Text',
-      'id' => 'text',
-    ])->save();
-
-    $field_storage = FieldStorageConfig::create([
-      'field_name' => 'field_text',
-      'entity_type' => 'paragraph',
-      'type' => 'string',
-      'cardinality' => '1',
-    ]);
-    $field_storage->save();
-    $field = FieldConfig::create([
-      'field_storage' => $field_storage,
-      'bundle' => 'text',
-    ]);
-    $field->save();
-
-    ParagraphsType::create([
-      'label' => 'Image',
-      'id' => 'image',
-    ])->save();
-
-    ParagraphsType::create([
-      'label' => 'Video',
-      'id' => 'video',
-    ])->save();
-
-    $media_type_oembed = $this->createMediaType('oembed:video', ['id' => 'remote_video', 'label' => 'Remote video']);
-    $this->createMediaType('image', ['id' => 'image', 'label' => 'Image']);
-
-    $field_storage = FieldStorageConfig::create([
-      'field_name' => 'field_entity_reference',
-      'entity_type' => 'paragraph',
-      'type' => 'entity_reference',
-      'cardinality' => '1',
-      'settings' => [
-        'target_type' => 'media',
-      ],
-    ]);
-    $field_storage->save();
-    $field = FieldConfig::create([
-      'field_storage' => $field_storage,
-      'bundle' => 'video',
-      'settings' => [
-        'handler_settings' => [
-          'target_bundles' => [
-            $media_type_oembed->id() => $media_type_oembed->id(),
-          ],
-        ],
-      ],
-    ]);
-    $field->save();
+    $this->installConfig(['paragraphs_paste_test']);
 
   }
 
@@ -106,7 +55,7 @@ class PropertyPathAutocompleteControllerTest extends KernelTestBase {
       ],
       [
         'paragraph.',
-        '[{"value":"paragraph.image.","label":"paragraph.image (Image)","keyword":"image Image"},{"value":"paragraph.text.","label":"paragraph.text (Text)","keyword":"text Text"},{"value":"paragraph.video.","label":"paragraph.video (Video)","keyword":"video Video"}]',
+        '[{"value":"paragraph.text.","label":"paragraph.text (Text)","keyword":"text Text"},{"value":"paragraph.video.","label":"paragraph.video (Video)","keyword":"video Video"}]',
       ],
       [
         'paragraph.te',
@@ -114,11 +63,11 @@ class PropertyPathAutocompleteControllerTest extends KernelTestBase {
       ],
       [
         'paragraph.text.',
-        '[{"value":"paragraph.text.field_text","label":"paragraph.text.field_text (field_text)","keyword":"field_text field_text"},{"value":"paragraph.text.parent_field_name","label":"paragraph.text.parent_field_name (Parent field name)","keyword":"parent_field_name Parent field name"},{"value":"paragraph.text.parent_id","label":"paragraph.text.parent_id (Parent ID)","keyword":"parent_id Parent ID"},{"value":"paragraph.text.parent_type","label":"paragraph.text.parent_type (Parent type)","keyword":"parent_type Parent type"}]',
+        '[{"value":"paragraph.text.field_text","label":"paragraph.text.field_text (Text)","keyword":"field_text Text"},{"value":"paragraph.text.parent_field_name","label":"paragraph.text.parent_field_name (Parent field name)","keyword":"parent_field_name Parent field name"},{"value":"paragraph.text.parent_id","label":"paragraph.text.parent_id (Parent ID)","keyword":"parent_id Parent ID"},{"value":"paragraph.text.parent_type","label":"paragraph.text.parent_type (Parent type)","keyword":"parent_type Parent type"}]',
       ],
       [
-        'paragraph.video.field_entity_reference:',
-        '[{"value":"paragraph.video.field_entity_reference:remote_video.","label":"paragraph.video.field_entity_reference:remote_video (Remote video)","keyword":"remote_video Remote video"}]',
+        'paragraph.video.field_video:',
+        '[{"value":"paragraph.video.field_video:remote_video.","label":"paragraph.video.field_video:remote_video (Remote video)","keyword":"remote_video Remote video"}]',
       ],
     ];
   }
