6898842d78d024be557146de687a2f3878846d43
Christian Fritsch <chr.fritsch@gmx.net>
2020-03-25 16:38:48 +0100

    fix: add config schema and test

---
+++ tests/src/Functional/ParagraphsPasteConfigTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Drupal\Tests\paragraphs\Functional;
+
+use Drupal\Tests\BrowserTestBase;
+
+/**
+ * Tests that Paragraphs paste config test.
+ *
+ * @group paragraphs_paste
+ */
+class ParagraphsPasteConfigTest extends BrowserTestBase {
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = ['paragraphs_paste_test'];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Tests that paragraphs_paste can be installed.
+   */
+  public function testInstall() {}
+
+}
