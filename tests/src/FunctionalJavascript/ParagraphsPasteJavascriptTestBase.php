db3d45eb66bec3cd30bbe628d2da3aa1affd6eb7
Volker Killesreiter <killesre@gmail.com>
2020-03-31 14:38:44 +0200

    refactor into base class

---
+++ tests/src/FunctionalJavascript/ParagraphsPasteJavascriptTestBase.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Drupal\Tests\paragraphs_paste\FunctionalJavascript;
+
+use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
+use Drupal\Tests\paragraphs\FunctionalJavascript\LoginAdminTrait;
+
+/**
+ * Base class for Javascript tests for paragraphs_paste module.
+ *
+ * @package Drupal\Tests\paragraphs_paste\FunctionalJavascript
+ */
+abstract class ParagraphsPasteJavascriptTestBase extends WebDriverTestBase {
+
+  use LoginAdminTrait;
+
+  /**
+   * Modules to enable.
+   *
+   * @var array
+   */
+  protected static $modules = [
+    'paragraphs_paste_test',
+    'field_ui',
+  ];
+
+  /**
+   * {@inheritdoc}
+   */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * Simulate paste event.
+   *
+   * @param string $text
+   *   Text to copy.
+   */
+  public function simulatePasteEvent($text) {
+    $jsScript = "var pasteData = new DataTransfer(); pasteData.setData('text/plain', '{$text}'); document.querySelector('.paragraphs-paste-action').dispatchEvent(new ClipboardEvent('paste', {clipboardData: pasteData}));";
+    $this->getSession()->executeScript($jsScript);
+  }
+
+  /**
+   * Wait for element to be present.
+   *
+   * @param string $selector
+   *   The CSS selector.
+   * @param int $timeout
+   *   (Optional) Timeout in milliseconds, defaults to 1000.
+   * @param string $message
+   *   (Optional) Message to pass to assertJsCondition().
+   */
+  public function waitForElementPresent($selector, $timeout = 1000, $message = '') {
+    $this->assertJsCondition("document.querySelector('" . $selector . "')", $timeout, $message);
+  }
+
+}
--- tests/src/FunctionalJavascript/ParagraphsPasteTest.php
+++ tests/src/FunctionalJavascript/ParagraphsPasteTest.php
@@ -8,28 +8,9 @@ use Drupal\Tests\paragraphs\FunctionalJavascript\LoginAdminTrait;
 /**
  * Tests the creation of paragraphs by pasting random data.
  *
- * TODO: Move widget settings form tests in separate class.
- *
- * @group paragraphs_features
+ * @group paragraphs_paste
  */
-class ParagraphsPasteTest extends WebDriverTestBase {
-
-  use LoginAdminTrait;
-
-  /**
-   * Modules to enable.
-   *
-   * @var array
-   */
-  protected static $modules = [
-    'paragraphs_paste_test',
-    'field_ui',
-  ];
-
-  /**
-   * {@inheritdoc}
-   */
-  protected $defaultTheme = 'stark';
+class ParagraphsPasteTest extends ParagraphsPasteJavascriptTestBase {
 
   /**
    * Test paste functionality.
@@ -46,14 +27,11 @@ class ParagraphsPasteTest extends WebDriverTestBase {
     // Check that paste functionality is working with default config.
     $text = "Lorem ipsum dolor sit amet.";
     $this->drupalGet("node/add/$content_type");
-    $this->assertEquals(TRUE, $driver->isVisible('//*[@data-paragraphs-paste-target="edit-field-paragraphs-paragraphs-paste-paste-action"]'), 'Paragraphs Paste area should be visible.');
+    $this->assertTrue($driver->isVisible('//*[@data-paragraphs-paste-target="edit-field-paragraphs-paragraphs-pa    ste-paste-action"]'), 'Paragraphs Paste area should be visible.');
 
-    $jsScript = "var pasteData = new DataTransfer(); pasteData.setData('text/plain', '{$text}'); document.querySelector('.paragraphs-paste-action').dispatchEvent(new ClipboardEvent('paste', {clipboardData: pasteData}));";
-    $session->executeScript($jsScript);
-    $this->assertJsCondition("document.querySelector('[data-drupal-selector=\"edit-field-paragraphs-0-subform-field-text-0-value\"]')", 10000, 'Text field in paragraph form should be visible.');
+    $this->simulatePasteEvent($text);
+    $this->waitForElementPresent('[data-drupal-selector="edit-field-paragraphs-0-subform-field-text-0-value"]', 10000, 'Text field in paragraph form should be present.');
     $this->assertEquals($text, $page->find('xpath', '//textarea[@data-drupal-selector="edit-field-paragraphs-0-subform-field-text-0-value"]')->getText(), 'Text should be pasted into paragraph subform.');
-
-#    $this->assertTrue(FALSE);
   }
 
 }
