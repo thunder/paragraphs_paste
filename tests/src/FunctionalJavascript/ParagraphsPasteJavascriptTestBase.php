05c8f8d32f39a679effce23610c8253f6ae6cf99
Christian Fritsch <chr.fritsch@gmx.net>
2020-04-03 11:29:33 +0200

    fix: fix notices and add a test to ensure that there are no new notices

--- tests/src/FunctionalJavascript/ParagraphsPasteJavascriptTestBase.php
+++ tests/src/FunctionalJavascript/ParagraphsPasteJavascriptTestBase.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\Tests\paragraphs_paste\FunctionalJavascript;
 
+use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\Unicode;
+use Drupal\dblog\Controller\DbLogController;
 use Drupal\FunctionalJavascriptTests\WebDriverTestBase;
 use Drupal\Tests\paragraphs\FunctionalJavascript\LoginAdminTrait;
 
@@ -22,6 +25,7 @@ abstract class ParagraphsPasteJavascriptTestBase extends WebDriverTestBase {
   protected static $modules = [
     'paragraphs_paste_test',
     'field_ui',
+    'dblog',
   ];
 
   /**
@@ -53,6 +57,39 @@ abstract class ParagraphsPasteJavascriptTestBase extends WebDriverTestBase {
    */
   public function waitForElementPresent($selector, $timeout = 1000, $message = '') {
     $this->assertJsCondition("document.querySelector('{$selector}')", $timeout, $message);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function tearDown() {
+    /** @var \Drupal\Core\Database\Query\SelectInterface $query */
+    $query = \Drupal::database()->select('watchdog', 'w')
+      ->fields('w', ['message', 'variables']);
+    $andGroup = $query->andConditionGroup()
+      ->condition('severity', 6, '<')
+      ->condition('type', 'php');
+    $group = $query->orConditionGroup()
+      ->condition('severity', 4, '<')
+      ->condition($andGroup);
+    $query->condition($group);
+    $query->groupBy('w.message');
+    $query->groupBy('w.variables');
+
+    $controller = DbLogController::create($this->container);
+
+    // Check that there are no warnings in the log after installation.
+    // $this->assertEqual($query->countQuery()->execute()->fetchField(), 0);.
+    if ($query->countQuery()->execute()->fetchField()) {
+      // Output all errors for modules tested.
+      $errors = [];
+      foreach ($query->execute()->fetchAll() as $row) {
+        $errors[] = Unicode::truncate(Html::decodeEntities(strip_tags($controller->formatMessage($row))), 256, TRUE, TRUE);
+      }
+      throw new \Exception(print_r($errors, TRUE));
+    }
+
+    parent::tearDown();
   }
 
 }
