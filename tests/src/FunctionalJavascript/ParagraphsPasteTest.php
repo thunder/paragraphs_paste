185ee365bfda47358118fc7b65c6d5c8c0d9a065
Volker Killesreiter <killesre@gmail.com>
2020-04-21 16:11:03 +0200

    remove sleep

--- tests/src/FunctionalJavascript/ParagraphsPasteTest.php
+++ tests/src/FunctionalJavascript/ParagraphsPasteTest.php
@@ -29,7 +29,7 @@ class ParagraphsPasteTest extends ParagraphsPasteJavascriptTestBase {
     $text = "Lorem ipsum dolor sit amet.";
     $this->drupalGet("node/add/$content_type");
     $this->assertTrue($driver->isVisible('//*[@data-paragraphs-paste-target="edit-field-paragraphs-paragraphs-paste-paste-action"]'), 'Paragraphs Paste area should be visible.');
-    sleep(1);
+
     $this->simulatePasteEvent('[data-paragraphs-paste-target="edit-field-paragraphs-paragraphs-paste-paste-action"]', $text);
     $this->waitForElementPresent('[data-drupal-selector="edit-field-paragraphs-0-subform-field-text-0-value"]', 10000, 'Text field in paragraph form should be present.');
     $this->assertEquals("<p>{$text}</p>", $page->find('xpath', '//textarea[@data-drupal-selector="edit-field-paragraphs-0-subform-field-text-0-value"]')->getValue(), 'Text should be pasted into paragraph subform.');
